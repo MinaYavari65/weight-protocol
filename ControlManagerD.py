9f34737599bb6d388730004492396a83b8e00e53
thecoder-lanc <damian.arellanes@lancaster.ac.uk>
2025-04-18 20:52:17 +0800

    New computer server to work with

---
+++ ControlManagerD.py
@@ -0,0 +1,66 @@
+from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler, HTTPServer
+import json
+import threading
+
+class Control:
+    def __init__(self, id, masters, slaves, ip, port):
+        self.id = id
+        self.masters = masters # Example {'O3': '192.168.0.1',...}
+        self.slaves = slaves # Example {'O1': '192.168.0.3',...}
+        self.ip = ip
+        self.port = port
+
+    def __str__(self):
+        return f"{self.ip}:{self.port}"
+
+def run_control_server(id, port):
+    class ControlServer(BaseHTTPRequestHandler):
+        def do_POST(self):
+            content_len = int(self.headers.get('content-length', 0))
+            post_body = self.rfile.read(content_len)
+            #print(f"Received POST request with body: {post_body}")
+            
+            # Compute weights        
+                    
+            # Send a response back to the client
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+
+    server_address = ('', port)
+    new_server = ThreadingHTTPServer(server_address, ControlServer)
+    print(f"Control {id} started on port {port}")
+    new_server.serve_forever()
+
+class ControlManager(BaseHTTPRequestHandler):   
+    def do_POST(self):
+        # Deserialise JSON message from deployer
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body_str = post_body.decode('utf-8')
+        data = json.loads(post_body_str)
+
+        # Start control server
+        for key, value in data.items():       
+            ControlManager.next_port += 1
+            control = Control(key, value['masters'], value['slaves'], self.server.server_address[0], self.next_port)              
+            t2 = threading.Thread(target=run_control_server, args=(control.id, control.port,))
+            t2.daemon = True  # daemon thread so it won't block program exit
+            t2.start()                    
+                
+        # Send a response back to the client
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+def run_server(ip, port):
+    server_address = (ip, port)
+    httpd = HTTPServer(server_address, ControlManager)
+    print('Starting Control Manager Server...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    ip = '127.0.0.1'
+    port = 8080
+    ControlManager.next_port = port
+    run_server(ip, port)
